a64f84cd3f8a486d8f91766724fcf888d2406593
David Vollmar <david@vollmar.nl>
2017-12-02 20:33:12 +0100

    You spin me right round like a mouse

--- app/curlemouse.js
+++ app/curlemouse.js
@@ -3,15 +3,19 @@ class CurleMouse {
 		this.sprite = game.add.sprite(x, y, "mouse");
         this.sprite.anchor.setTo(0.5, 0.5);
         this.sprite.scale.setTo(0.05, 0.05);
-
-        this.sprite.rotation = rotation;
+        // rotation for rendering
+        this.sprite.rotation = rotation; 
+        // rotation relative to centre of the world
+        this.rotation = rotation;
+        
         this.distance = 75;
         this.speed = 8;
     }
 
     move() {
     	this.distance += this.speed;
-    	this.sprite.x = game.world.centerX + this.distance * Math.cos(this.sprite.rotation);
-    	this.sprite.y = game.world.centerY + this.distance * Math.sin(this.sprite.rotation);
+    	this.sprite.x = game.world.centerX + this.distance * Math.cos(this.rotation);
+        this.sprite.y = game.world.centerY + this.distance * Math.sin(this.rotation);
+        this.sprite.rotation += 0.2;
     }
 }
