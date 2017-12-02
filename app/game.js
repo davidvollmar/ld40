46bccf2ab38d83bc5a1fb9e38f3e44ce633cb09f
David Vollmar <david@vollmar.nl>
2017-12-02 12:10:09 +0100

    Adds cheesiness

--- app/game.js
+++ app/game.js
@@ -1,17 +1,21 @@
-console.log("hiero");
 window.onload = function() {
-	console.log("here!");
+	console.log("Prepare to Cheese!");
 
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create });
-
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create, update: update});
+	
 	function preload () {
-		game.load.image("logo", "phaser.png");
-
+		game.load.image("cheese", "assets/cheese.png");
 	}
 
 	function create () {
-		var logo = game.add.sprite(game.world.centerX, game.world.centerY, "logo");
-		logo.anchor.setTo(0.5, 0.5);
+		var cheese = game.add.sprite(game.world.centerX, game.world.centerY, "cheese");
+		cheese.scale.setTo(0.1, 0.1);
+		cheese.anchor.setTo(0.5, 0.5);
+
+		console.log("Cheese!");
+	}
+
+	function update() {
 
 	}
 };
