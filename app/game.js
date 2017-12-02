12f7651fb5010d1bc47e129122b20ff659735932
David Vollmar <david@vollmar.nl>
2017-12-02 11:30:00 +0100

    Added phaser

---
+++ app/game.js
@@ -0,0 +1,17 @@
+console.log("hiero");
+window.onload = function() {
+	console.log("here!");
+
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create });
+
+	function preload () {
+		game.load.image("logo", "phaser.png");
+
+	}
+
+	function create () {
+		var logo = game.add.sprite(game.world.centerX, game.world.centerY, "logo");
+		logo.anchor.setTo(0.5, 0.5);
+
+	}
+};
